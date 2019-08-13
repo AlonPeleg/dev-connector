f223a4bb4a0d4fc4254488813c762f8aee94d5eb
Alon Peleg <peleg619@gmail.com>
2019-08-14 02:23:50 +0300

    getprofile , profile error

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -12,6 +12,18 @@ export default function(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      };
     default:
       return state;
   }

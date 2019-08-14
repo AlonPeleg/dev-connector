7a156eb1fe3b74b53129aaa4fcf0dcdb25948b82
Alon Peleg <peleg619@gmail.com>
2019-08-15 02:37:02 +0300

    Add Experience/Education

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import PrivateRoute from "./components/routing/PrivateRoute";
 import CreateProfile from "./components/profile-form/CreateProfile";
 import EditProfile from "./components/profile-form/EditProfile";
 import AddExperience from "./components/profile-form/AddExperience";
+import AddEducation from "./components/profile-form/AddEducation";
 
 // Redux
 import { Provider } from "react-redux";
@@ -54,6 +55,11 @@ const App = () => {
                 exact
                 path="/add-experience"
                 component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
               />
             </Switch>
           </section>
